865ddb191cadd940914b771c9e658f9ea8ffba6f
danju4rizzl <danjumashiwaju@gmail.com>
2023-04-15 12:09:26 +0200

    added responsive slider

--- main.js
+++ main.js
@@ -4,11 +4,36 @@ const counters = document.querySelectorAll(".counter")
 let counted = false
 
 const swiper = new Swiper("#cardSwiper", {
-	slidesPerView: 3,
-	spaceBetween: 80,
-	autoPlay: true,
+	slidesPerView: 1,
+	spaceBetween: 30,
 
-	loop: true
+	autoPlay: {
+		delay: 2500
+	},
+
+	loop: true,
+
+	breakpoints: {
+		// when window width is >= 600px
+		600: {
+			slidesPerView: 1,
+			spaceBetween: 40
+		},
+		// when window width is >= 900px
+		900: {
+			spaceBetween: 20
+		},
+		// when window width is >= 1263px
+		1263: {
+			slidesPerView: 2,
+			spaceBetween: 30
+		},
+		// when window width is >= 1400px
+		1400: {
+			slidesPerView: 3,
+			spaceBetween: 35
+		}
+	}
 })
 
 // counter function
@@ -21,7 +46,7 @@ const counter = (element, amount) => {
 		} else {
 			clearInterval(interval)
 		}
-	}, 10)
+	}, 50)
 }
 
 // check if element is in viewport
@@ -37,23 +62,21 @@ function isInViewport(el) {
 }
 
 // Will run when the page is scrolled then check if the counter is in the viewport and if the counter was already counted or not
-function runCounter() {
-	window.addEventListener("scroll", function () {
-		if (!counted && isInViewport(counters[0])) {
-			counted = true
-			counters.forEach((counterElement) => {
-				counter(counterElement, counterElement.getAttribute("data-count"))
-			})
-			console.log("In viewport")
-		} else {
-			console.log("Not in viewport")
-			return
-		}
-	})
-}
+window.addEventListener("scroll", function () {
+	if (!counted && isInViewport(counters[0])) {
+		counted = true
+		counters.forEach((counterElement) => {
+			counter(counterElement, counterElement.getAttribute("data-deejay"))
+		})
+		console.log("In viewport")
+	} else {
+		console.log("Not in viewport")
+		return
+	}
+})
 
 // This will show the current year in the footer
-function showCurrentYear(el) {
+function showCurrentYear() {
 	const currentYear = document.querySelector("#currentYear")
 	const year = new Date().getFullYear()
 	currentYear.innerHTML = year
@@ -61,6 +84,5 @@ function showCurrentYear(el) {
 
 // Runs when the page is loaded
 window.addEventListener("load", () => {
-	runCounter()
 	showCurrentYear()
 })
